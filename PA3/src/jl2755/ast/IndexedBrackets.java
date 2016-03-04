15d3e2aeccba3fdb147b4f66a7146b93416e9d07
ttl33 <ttl33@cornell.edu>
2016-03-04 17:25:15 -0500

    comments for brackets related classes in ast

--- PA3/src/jl2755/ast/IndexedBrackets.java
+++ PA3/src/jl2755/ast/IndexedBrackets.java
@@ -3,6 +3,12 @@ package jl2755.ast;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents brackets with contents.
+ * index
+ * 	- 0: [e]
+ * 	- 1: [e]indexedBrackets
+ */
 public class IndexedBrackets {
 
 	private Expr expression;
@@ -20,6 +26,9 @@ public class IndexedBrackets {
 		index = 1;
 	}
 	
+	/**
+	 * @return a List of expression inside each bracket pair
+	 */
 	public List<Expr> getContent() {
 		List<Expr> tempList = new ArrayList<Expr>();
 		if (index == 0){
@@ -33,6 +42,9 @@ public class IndexedBrackets {
 		}
 	}
 
+	/**
+	 * @return a total number of bracket pairs
+	 */
 	public int getNumBrackets() {
 		return getContent().size();
 	}
--- PA3/src/jl2755/ast/MixedBrackets.java
+++ PA3/src/jl2755/ast/MixedBrackets.java
@@ -6,6 +6,13 @@ import java.util.List;
 import edu.cornell.cs.cs4120.util.CodeWriterSExpPrinter;
 import jl2755.GlobalPrettyPrinter;
 
+/**
+ * Represents indexed brackets & indexed brackets followed by empty brackets
+ * index
+ * 	- 0: [e]
+ * 	- 1: [e]mixedBrackets
+ * 	- 2: [e]brackets
+ */
 public class MixedBrackets {
 	private Expr e;
 	private MixedBrackets mixedBrackets;
@@ -30,6 +37,10 @@ public class MixedBrackets {
 		setIndex(2);
 	}
 	
+	/**
+	 * @return a List containing expressions of only indexed brackets
+	 * ex) [e1][e2][][][] -> {e1,e2}
+	 */
 	public List<Expr> getContent() {
 		List<Expr> list = new ArrayList<Expr>();
 		list.add(e);
@@ -39,6 +50,9 @@ public class MixedBrackets {
 		return list;
 	}
 	
+	/**
+	 * @return a total number of bracket pairs (indexed+empty)
+	 */
 	public int getNumBrackets(){
 		if (index == 0) {
 			return 1 + mixedBrackets.getNumBrackets();

0bac92aab055a6a02a7298c3727d5fcf2209edff
Ju Hee Lee <juhee0202@gmail.com>
2016-04-09 01:36:50 -0400

    merge

--- PA5/src/jl2755/visitor/TilingVisitor.java
+++ PA5/src/jl2755/visitor/TilingVisitor.java
@@ -32,56 +32,9 @@ public class TilingVisitor implements IRTreeVisitor {
 	/** Lists of strings representing possible tiles. */
 	// TODO: put all these in a json file and read the json file to populate patternMap
 
-	private static final List<String> BINOP1_PRE = new ArrayList<String>(
-			Arrays.asList(
-					"Binop"
-					));
-	private static final List<String> BINOP1_IN = new ArrayList<String>(
-			Arrays.asList(
-					"Binop"
-					));
-	private static final List<String> BINOP2_PRE = new ArrayList<String>(
-			Arrays.asList(
-					"Binop",
-					"Mem"
-					));
-	private static final List<String> BINOP2_IN = new ArrayList<String>(
-			Arrays.asList(
-					"Mem",
-					"Binop"
-					));
-	private static final List<String> BINOP3_PRE = new ArrayList<String>(
-			Arrays.asList(
-					"Binop",
-					"Mem"
-					));
-	private static final List<String> BINOP3_IN = new ArrayList<String>(
-			Arrays.asList(
-					"Binop",
-					"Mem"
-					));
-	private static final List<String> BINOP4_PRE = new ArrayList<String>(
-			Arrays.asList(
-					"Binop",
-					"Const"
-					));
-	private static final List<String> BINOP4_IN = new ArrayList<String>(
-			Arrays.asList(
-					"Const",
-					"Binop"
-					));
-	private static final List<String> BINOP5_PRE = new ArrayList<String>(
-			Arrays.asList(
-					"Binop",
-					"Const",
-					"Mem"
-					));
-	private static final List<String> BINOP5_IN = new ArrayList<String>(
-			Arrays.asList(
-					"Const",
-					"Binop",
-					"Mem"
-					));
+//	// BINOP
+//	private static final List<String> BINOP = new ArrayList<String>(
+//			Arrays.asList("BINOP"));
 
 	// CJUMP
 	private static final List<String> CJUMP_BINOP_PRE = new ArrayList<String>(
@@ -221,6 +174,7 @@ public class TilingVisitor implements IRTreeVisitor {
 		BinOpTile binOp;
 		switch(op) {
 		case ADD: 
+			
 		case SUB:
 		case MUL:
 		case HMUL:
@@ -252,7 +206,7 @@ public class TilingVisitor implements IRTreeVisitor {
 			// cmp
 		}
 	}
-
+	
 	@Override
 	public void visit(IRCall call) {
 		// TODO Auto-generated method stub

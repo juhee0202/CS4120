b173d7c3f4c640a6c7bf01c462188f0c629af548
ttl33 <ttl33@cornell.edu>
2016-04-30 00:16:31 -0400

    idom fix

--- PA6/src/edu/cornell/cs/cs4120/xic/ir/IRPhiFunction.java
+++ PA6/src/edu/cornell/cs/cs4120/xic/ir/IRPhiFunction.java
@@ -3,6 +3,7 @@ package edu.cornell.cs.cs4120.xic.ir;
 
 import edu.cornell.cs.cs4120.util.SExpPrinter;
 import jl2755.assembly.ChildType;
+import jl2755.controlflow.CFGNode;
 import jl2755.visitor.IRTreeVisitor;
 
 /**
@@ -16,11 +17,13 @@ public class IRPhiFunction extends IRStmt {
 	private String originalVar;
 	private String var;
 	private String[] operands;
+	private CFGNode[] insertionPoints;
 	
 	public IRPhiFunction(String argVar, int n) {
 		originalVar = argVar;
 		var = argVar;
 		operands = new String[n];
+		insertionPoints = new CFGNode[n];
 	}
 	
 	public String getOriginalVar() {
@@ -37,6 +40,10 @@ public class IRPhiFunction extends IRStmt {
 	
 	public String[] getOperands() {
 		return operands;
+	}
+	
+	public void setInsertionPoint(int i, CFGNode insertionPoint) {
+		insertionPoints[i] = insertionPoint;
 	}
 	
 	public void setOperand(int i, String operand) {
@@ -59,6 +66,14 @@ public class IRPhiFunction extends IRStmt {
 
 	@Override
 	public void accept(IRTreeVisitor irv) {
+	}
+
+	public CFGNode[] getInsertionPoints() {
+		return insertionPoints;
+	}
+
+	public void setInsertionPoints(CFGNode[] insertionPoints) {
+		this.insertionPoints = insertionPoints;
 	}
 
 	@Override

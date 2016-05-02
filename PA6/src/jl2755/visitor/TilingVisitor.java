127cbb0882e4a1429c82dc3816fd96406e1408c0
Ju Hee Lee <juhee0202@gmail.com>
2016-05-01 20:14:19 -0400

    FIXED MULTIPLE RETURN: we should not have been pushing rsp onto the stack

--- PA6/src/jl2755/visitor/TilingVisitor.java
+++ PA6/src/jl2755/visitor/TilingVisitor.java
@@ -40,7 +40,7 @@ public class TilingVisitor implements IRTreeVisitor {
 	/** list of caller-saved registers */
 	// TODO: temporarily added rdi and rsi here but i should refactor.
 	private static final String[] CALLER_REG_LIST = {
-			"rax", "rcx", "rdx", "r8", "r9", "r10", "r11", "rdi", "rsi", "rsp"
+			"rax", "rcx", "rdx", "r8", "r9", "r10", "r11", "rdi", "rsi"
 	};
 	// shuttle regs: rcx, rdx, r11
 	
@@ -754,20 +754,6 @@ public class TilingVisitor implements IRTreeVisitor {
 			Register rdi = new Register(RegisterName.RDI);
 			Register rsp = new Register(RegisterName.RSP);
 			Register temp = new Register(RegisterName.R11);
-			// "movq rsp temp"
-			Instruction makeTempForRsp = new Instruction(Operation.MOVQ, rsp, temp);
-			// "subq 8 temp"
-			Instruction computeRet3MemAddr = new Instruction(Operation.SUBQ, new Constant(8), temp);
-			// "movq temp rdi"
-			Instruction moveRet3MemAddr = new Instruction(Operation.MOVQ, temp, rdi);
-			tempInstructions.add(makeTempForRsp);
-			tempInstructions.add(computeRet3MemAddr);
-			tempInstructions.add(moveRet3MemAddr);
-			
-//			Memory ret3 = new Memory(new Constant(-8), rsp); 
-//			// "movq ret3 rdi"
-//			Instruction instr = new Instruction(Operation.MOVQ, ret3, rdi);
-//			tempInstructions.add(instr);
 			
 			// create (m-2) words space 
 			Constant c = new Constant(8*(numReturns - 2));
@@ -776,6 +762,16 @@ public class TilingVisitor implements IRTreeVisitor {
 			tempInstructions.add(instr2);
 			numArgRegs--;	// because rdi is used to store ret3
 			num8ByteSpace += numReturns - 2;
+			
+			// "movq rsp rdi"
+			Instruction makeTempForRsp = new Instruction(Operation.MOVQ, rsp, rdi);
+			tempInstructions.add(makeTempForRsp);
+			// "addq 8*(m-2)-8 rdi"
+			Constant offsetFromRsp = new Constant(8*(numReturns-2) - 8);
+			if (offsetFromRsp.getValue() != 0) {
+				Instruction computeRet3MemAddr = new Instruction(Operation.ADDQ, offsetFromRsp, rdi);
+				tempInstructions.add(computeRet3MemAddr);
+			}
 		}
 		
 		/* Push argn...arg7 (or arg6 if m > 2) */

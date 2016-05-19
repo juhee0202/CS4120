d625804fd1fcf673ad53c277b1e44646f46e4490
Sung Won An <sa579@cornell.edu>
2016-05-18 21:23:59 -0400

    irglobalreference fixes

---
+++ tests/customPA7/global1.s
@@ -0,0 +1,82 @@
+	.text
+	.globl	_Imain_paai
+	.align	4
+_Imain_paai:
+	enter	$80, $0
+	movq	%rbx, -8(%rbp)
+	movq	%rbp, -16(%rbp)
+	movq	%r12, -24(%rbp)
+	movq	%r13, -32(%rbp)
+	movq	%r14, -40(%rbp)
+	movq	%r15, -48(%rbp)
+	movq	%rdi, %r12
+	movq	%r12, -56(%rbp)
+	movq	_I_g_x_i(%rip), %r14
+	movq	%r14, -64(%rbp)
+	pushq	%rax
+	pushq	%rcx
+	pushq	%rdx
+	pushq	%r8
+	pushq	%r9
+	pushq	%r10
+	pushq	%r11
+	pushq	%rdi
+	pushq	%rsi
+	movq	-64(%rbp), %r14
+	movq	%r14, %rdi
+	subq	$8, %rsp
+	callq	_IunparseInt_aii
+	movq	%rax, %r12
+	movq	%r12, -72(%rbp)
+	addq	$8, %rsp
+	popq	%rsi
+	popq	%rdi
+	popq	%r11
+	popq	%r10
+	popq	%r9
+	popq	%r8
+	popq	%rdx
+	popq	%rcx
+	popq	%rax
+	movq	-72(%rbp), %r14
+	movq	%r14, %r12
+	movq	%r12, -80(%rbp)
+	pushq	%rax
+	pushq	%rcx
+	pushq	%rdx
+	pushq	%r8
+	pushq	%r9
+	pushq	%r10
+	pushq	%r11
+	pushq	%rdi
+	pushq	%rsi
+	movq	-80(%rbp), %r14
+	movq	%r14, %rdi
+	subq	$8, %rsp
+	callq	_Iprintln_pai
+	addq	$8, %rsp
+	popq	%rsi
+	popq	%rdi
+	popq	%r11
+	popq	%r10
+	popq	%r9
+	popq	%r8
+	popq	%rdx
+	popq	%rcx
+	popq	%rax
+	movq	-8(%rbp), %rbx
+	movq	-16(%rbp), %rbp
+	movq	-24(%rbp), %r12
+	movq	-32(%rbp), %r13
+	movq	-40(%rbp), %r14
+	movq	-48(%rbp), %r15
+	leave
+	ret
+
+
+	.section .data
+	.align 8
+.globl _I_g__I_g_x_i
+_I_g__I_g_x_i:
+	.quad 5
+	.text
